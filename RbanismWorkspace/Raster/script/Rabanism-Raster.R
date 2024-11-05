ebd5750b606869c1048f835384b3cbcb465086f3
Maas <massoud.ghaderian@outlook.com>
2024-11-05 18:48:24 +0100

    script update

---
+++ RbanismWorkspace/Raster/script/Rabanism-Raster.R
@@ -0,0 +1,32 @@
+#install libraries
+install.packages(c("tidyverse", "terra", "viridis"))   
+library(tidyverse)
+library(terra)
+library(viridis)
+
+
+# View Raster File Attributes ---------------------------------------------
+describe("D:/R-WorkSpace/R-30dayMapChallange/RbanismWorkspace/Raster/data/DSM5m.tif")
+
+
+
+# Open a Raster in R ------------------------------------------------------
+
+DSM_TUD <- rast("D:/R-WorkSpace/R-30dayMapChallange/RbanismWorkspace/Raster/data/DSM5m.tif")
+DSM_TUD
+
+
+summary(DSM_TUD)
+
+
+summary(values(DSM_TUD))
+
+
+DSM_TUD_df <- as.data.frame(DSM_TUD, xy=TRUE)
+str(DSM_TUD_df)
+
+
+ggplot() +
+  geom_raster(data = DSM_TUD_df , aes(x = x, y = y, fill = `DSM5m`)) +
+  scale_fill_viridis_c(option = "H") +  # `option = "H"` provides a contrasting colour scale
+  coord_quickmap() 
