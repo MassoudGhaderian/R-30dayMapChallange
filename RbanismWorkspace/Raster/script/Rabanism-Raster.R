290b251ad7c221d14ebd531c83a95218b534cb5d
Maas <massoud.ghaderian@outlook.com>
2024-11-06 11:33:21 +0100

    final out put

--- RbanismWorkspace/Raster/script/Rabanism-Raster.R
+++ RbanismWorkspace/Raster/script/Rabanism-Raster.R
@@ -26,11 +26,60 @@ DSM_TUD <- project(DSM_TUD, "EPSG:4326")
 
 DSM_TUD_df <- as.data.frame(DSM_TUD, xy=TRUE)
 str(DSM_TUD_df)
+colnames(DSM_TUD_df) <- c("x", "y", "elevation")
 colnames(DSM_TUD_df)
 
 
 ggplot() +
-  geom_raster(data = DSM_TUD_df , aes(x = x, y = y, fill = `DSM5m`)) + # Use actual column name if different
+  geom_raster(data = DSM_TUD_df , aes(x = x, y = y, fill = `elevation`)) + # Use actual column name if different
   scale_fill_viridis_c(option = "H") +
   coord_quickmap()
 
+
+DSM_TUD_df <- DSM_TUD_df %>%
+  mutate(fct_elevation = cut(`elevation`, breaks = 3))
+
+ggplot() +
+  geom_bar(data = DSM_TUD_df, aes(fct_elevation))
+
+levels(DSM_TUD_df$fct_elevation)
+DSM_TUD_df %>% 
+  count(fct_elevation)
+
+custom_bins <- c(-10, 0, 5, 100)
+
+DSM_TUD_df <- DSM_TUD_df %>%
+  mutate(fct_elevation_cb = cut(`elevation`, breaks = custom_bins))
+
+levels(DSM_TUD_df$fct_elevation_cb)
+
+ggplot() +
+  geom_bar(data = DSM_TUD_df, aes(fct_elevation_cb))
+
+
+DSM_TUD_df %>% 
+  count(fct_elevation_cb)
+
+terrain.colors(3)
+
+ggplot() +
+  geom_raster(data = DSM_TUD_df , aes(x = x, y = y, fill = fct_elevation_cb)) + 
+  scale_fill_manual(values = terrain.colors(3)) + 
+  coord_quickmap()
+
+
+my_col <- terrain.colors(3)
+
+ggplot() +
+  geom_raster(data = DSM_TUD_df , aes(x = x, y = y,
+                                      fill = fct_elevation_cb)) + 
+  scale_fill_manual(values = my_col, name = "Elevation") + 
+  coord_quickmap()
+
+
+ggplot() +
+  geom_raster(data = DSM_TUD_df , aes(x = x, y = y,
+                                      fill = fct_elevation_cb)) + 
+  scale_fill_manual(values = my_col, name = "Elevation") +
+  theme(axis.title = element_blank()) + 
+  coord_quickmap()
