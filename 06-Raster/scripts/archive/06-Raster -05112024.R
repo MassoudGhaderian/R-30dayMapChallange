27ab1661b221809c9315075496a95559fd1364a9
Maas <massoud.ghaderian@outlook.com>
2024-11-07 12:59:22 +0100

    new edit

--- 06-Raster/scripts/archive/06-Raster -05112024.R
+++ 06-Raster/scripts/archive/06-Raster -05112024.R
@@ -1,6 +1,5 @@
 
 
-
 # Step 1: Load Required Libraries ----------------------------------------
 #install libraries
 install.packages(c("raster","ggplot2","magick","grid"))   
@@ -9,49 +8,44 @@ install.packages(c("raster","ggplot2","magick","grid"))
 ## for raster mapping
 library(raster)
 library(ggplot2)
-library(dplyr)
-
 ## for logo
 library(magick)
 library(grid)
 
 # Step 2: Load the DSM Raster Data ----------------------------------------
 
+
+# Load DSM file
 dsm <- raster("D:/R-WorkSpace/R-30dayMapChallange/06-Raster/data/DSM5m.tif")
 
+#review and check DSM file
 # Review Raster -----------------------------------------------------------
 
-res(dsm) # Cell size
-dim(dsm)# Dimensions
-extent(dsm) # Extent
-crs(dsm)# Coordinate Reference System
-ncell(dsm)# Number of cells
-summary(dsm)
+print(res(dsm)) # Cell size
+print(dim(dsm))# Dimensions
+print(extent(dsm)) # Extent
+print(crs(dsm))# Coordinate Reference System
+print(ncell(dsm))# Number of cells
 
-# Step 3: Plot  Map ------------------------------------------------
+# Step 3: Plot a Basic Map ------------------------------------------------
 
+
+#Using plot:
+plot(dsm, main = "DSM - Elevation Map", col = terrain.colors(100))
+
+
+#Using ggplot2:
 # Convert DSM raster to a data frame for ggplot
 dsm_df <- as.data.frame(dsm, xy = TRUE)
 colnames(dsm_df) <- c("x", "y", "elevation")
 
-
-# Define custom color palette and bins
-custom_colors <- c("black","white", "lightblue","deepskyblue","blue",
-                    "orange", "red", "darkred")         
-custom_bins <- c(100,50,30,20,10,5,0,-5,-10)
-
-dsm_df <- dsm_df %>%
-  mutate(fct_elevation_cb = cut(`elevation`, breaks = custom_bins))
-
-levels(dsm_df$fct_elevation_cb)
-
+# Plot with ggplot2
 
 rbanism_logo <- image_read('https://rbanism.org/assets/imgs/about/vi_l.jpg') # Download our logo
 
-#plotting by ggplot
-ggplot(dsm_df, aes(x = x, y = y, fill = fct_elevation_cb)) +
-  scale_fill_manual(values = custom_colors) +
+ggplot(dsm_df, aes(x = x, y = y, fill = elevation)) +
   geom_raster() +
+  scale_fill_viridis_c() +
   theme_minimal() +
   labs( title = "Elevation Map of TU Delft by Digital Surface Model (DSM) ",
         fill = "Elevation (m)",
@@ -59,7 +53,7 @@ ggplot(dsm_df, aes(x = x, y = y, fill = fct_elevation_cb)) +
         y = NULL,  # Remove y-axis title
         caption = "#30DayMapChallenge| Data Source: PDOK-DSM | Map by Massoud Ghaderian, 2024")+
   theme(
-    legend.position = "right", # Move legend below the plot
+    legend.position = "bottom", # Move legend below the plot
     axis.text = element_text(size = 8), # Customize axis text
     panel.grid = element_blank(), # Remove grid lines
     plot.title = element_text(face = "bold", size = 14, hjust = 0.5, margin = margin(b = 0.5)), # Center title
@@ -72,5 +66,4 @@ grid.raster(rbanism_logo, x = 0.9, y=0.9,  # x and y determine the position of t
 
 
 # Save the plot as a PNG file
-ggsave("TUDelftDSM.pdf", width =  8.27 , height = 10, dpi = 600)
-
+ggsave("TUDelftDSM.png", width = 10, height = 8, dpi = 120)
